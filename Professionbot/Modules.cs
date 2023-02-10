333f33aabcec960c379c9b550f88b46caaa04469
patri <patri@PATRICK>
2023-02-10 22:47:28 +0100

    Projektdateien hinzufügen.

---
+++ Professionbot/Modules.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Professionbot
+{
+
+    // Keep in mind your module **must** be public and inherit ModuleBase.
+    // If it isn't, it will not be discovered by AddModulesAsync!
+    public class Modules : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
+               => ReplyAsync(echo);
+    }
+
+    [Group("sample")]
+    public class SampleModule : ModuleBase<SocketCommandContext>
+    {
+        // ~sample square 20 -> 400
+        [Command("square")]
+        [Summary("Squares a number.")]
+        public async Task SquareAsync(
+            [Summary("The number to square.")]
+        int num)
+        {
+            // We can also access the channel from the Command Context.
+            await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+        }
+
+        // ~sample userinfo --> foxbot#0282
+        // ~sample userinfo @Khionu --> Khionu#8708
+        // ~sample userinfo Khionu#8708 --> Khionu#8708
+        // ~sample userinfo Khionu --> Khionu#8708
+        // ~sample userinfo 96642168176807936 --> Khionu#8708
+        // ~sample whois 96642168176807936 --> Khionu#8708
+        [Command("userinfo")]
+        [Summary
+        ("Returns info about the current user, or the user parameter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync(
+            [Summary("The (optional) user to get info from")]
+        SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+    }
+
+    [Group("admin")]
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Group("clean")]
+        public class CleanModule : ModuleBase<SocketCommandContext>
+        {
+            // ~admin clean
+            [Command]
+            public async Task DefaultCleanAsync()
+            {
+                // ...
+            }
+
+            // ~admin clean messages 15
+            [Command("messages")]
+            public async Task CleanAsync(int count)
+            {
+                // ...
+            }
+        }
+        // ~admin ban foxbot#0282
+        [Command("ban")]
+        public Task BanAsync(IGuildUser user) =>
+            Context.Guild.AddBanAsync(user);
+    }
+}
---
+++ Professionbot/Program.cs
@@ -0,0 +1,157 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Professionbot;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System.Linq;
+using Discord.Rest;
+using Microsoft.VisualBasic;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+    
+
+    private DiscordSocketClient _client;
+    public CommandService _commands;
+    private IServiceProvider _services;
+
+    private SocketGuild guild;
+
+    private ulong LogChannelID;
+    private SocketTextChannel Logchannel;
+    public List<string[]> players = new List<string[]>();
+    public async Task MainAsync()
+    {
+
+        DiscordSocketConfig config = new DiscordSocketConfig()
+        {
+            GatewayIntents = GatewayIntents.All
+        };
+
+        _client = new DiscordSocketClient(config);
+        _commands = new CommandService();
+        _services = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_commands)
+            .BuildServiceProvider();
+
+        var token = "MTA3MzU4ODYzMDYwMzQ1MjQ0Nw.G_fqLT.662P2OphuEtD1ynZG2CGFt7OayMqM7qhLu3Ijg";
+        _client.Log += _client_Log;
+        await RegisterCommandsAsync();
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        await Task.Delay(-1);
+    }
+
+    private Task _client_Log(LogMessage arg)
+    {
+        Console.WriteLine(arg);
+        return Task.CompletedTask;
+    }
+
+    public async Task RegisterCommandsAsync()
+    {
+        _client.MessageReceived += HandleCommandAsync;
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+
+    public async Task HandleCommandAsync(SocketMessage arg)
+    {
+        var message = arg as SocketUserMessage;
+        var context = new SocketCommandContext(_client, message);
+        var Author = message.Author;
+        var channel = _client.GetChannel(LogChannelID) as SocketTextChannel;
+
+        Console.WriteLine("---------------\nUser: " + message.Author.Username + " mit ID " + message.Author.Id + "\nhat geschrieben:" + "\n" +  message.ToString());
+        if (message.Author.IsBot) return;
+        int argPos = 0;
+        if (message.HasStringPrefix("/",ref argPos))
+        {
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        var text = message.ToString().ToLower();
+
+        if (text == "/berufe")
+        {
+            ulong messageChannelId = message.Channel.Id;
+            
+            var textChannel = _client.GetChannel(messageChannelId) as SocketTextChannel;
+            guild = _client.GetGuild(textChannel.Category.GuildId);
+            string msg = "`Spieler      Beruf      Spezialisierung      Notizen\n";
+            if (players.Count == 0)
+            {
+                await textChannel.SendMessageAsync("Keine Chars eingetragen!");
+                message.DeleteAsync();
+                return;
+            }
+            for(int i = 0; i < players.Count; i++)
+            {
+                msg += players[i][0].PadRight(12) + " " + players[i][1].PadRight(10) + " " + players[i][2].PadRight(20) + " " + players[i][3] + "\n";
+            }
+            msg += "`";
+            await textChannel.SendMessageAsync(msg);
+
+            message.DeleteAsync();
+        }
+        if (text.StartsWith("/add"))
+        {
+            if (arg.ToString().Split(" ").Length < 4)
+            {
+                await Author.SendMessageAsync("Falsche Anzahl an Argumenten:\n/add <Charactername> <Beruf> <Spezialisierung>\noder\nadd <Charactername> <Beruf> <Spezialisierung> <Notes>");
+            }else
+            {
+
+                if (arg.ToString().Split(" ").Length == 4)
+                    players.Add(new string[4] { arg.ToString().Split(" ")[1], arg.ToString().Split(" ")[2], arg.ToString().Split(" ")[3], "NA" });
+                else
+                {
+                    int argLentgth = arg.ToString().Length;
+                    int noteIndex = arg.ToString().IndexOf(arg.ToString().Split(" ")[4]);
+                    players.Add(new string[4] { arg.ToString().Split(" ")[1], arg.ToString().Split(" ")[2], arg.ToString().Split(" ")[3], arg.ToString().Substring(noteIndex, argLentgth-noteIndex) });
+                }
+                await Author.SendMessageAsync("Spieler **" + arg.ToString().Split(" ")[1] + "** mit Beruf **" + arg.ToString().Split(" ")[2] + "** hinzugefügt!");
+            }
+            
+
+            message.DeleteAsync();
+        }
+        if (text == "/help")
+        {
+            await Author.SendMessageAsync("**/Berufe** \n    *Gibt Liste mit allen Chars und deren Berufen zurück*\n-------------------------------------------------------"
+                + "\n**/add <Charactername> <Beruf> <Spezialisierung> <Notes>**"
+                + "\n    *Fügt Character zur Berufsliste hinzu*"
+                +"\n`Beruf    : Spezialiserungen"
+                + "\n_______________________________________________________"
+                + "\nVZ       : All"
+                + "\nJuwe     : Schmuck, Gems, All"
+                + "\nSchneider: All"
+                + "\nSchmied  : Waffen, Rüstungen, All"
+                + "\nLederer  : Ketten, Leder, All"
+                + "\nIngi     : All"
+                + "\nInschrift: Waffen, Trinkets, Treatise, All`"
+                + "\n-------------------------------------------------------"
+                + "\n**/request <Beruf> <Spezialisierung>**"
+                + "\n*    Pingt alle mit passendem Beruf und Spezialierung an*");
+
+
+
+            message.DeleteAsync();
+        }
+    }
+}
+
